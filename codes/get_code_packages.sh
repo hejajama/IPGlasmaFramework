acb95299edf7843c47ec0936cd8ec24a80209977
Heikki Mäntysaari <heikki.mantysaari@jyu.fi>
2026-01-28 13:05:10 +0200

    Fix local temporary disc space usage

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -4,14 +4,14 @@
 
 # download IPGlasma
 rm -fr ipglasma_code
-git clone --depth=5 https://github.com/chunshen1987/ipglasma -b ipglasma_jimwlk ipglasma_code
-(cd ipglasma_code; git checkout e37b9f191c3e5be00edb3c298348a6c7aeb73e6a)
+git clone --depth=5 git@github.com:hejajama/ipglasma.git -b wilson_line_output_directory ipglasma_code
+#(cd ipglasma_code; git checkout e37b9f191c3e5be00edb3c298348a6c7aeb73e6a)
 rm -fr ipglasma_code/.git
 
 # download subnucleondiffraction
 rm -fr subnucleondiffraction_code
-#git clone --depth=1 https://github.com/hejajama/subnucleondiffraction subnucleondiffraction_code
-git clone --depth=1 https://github.com/chunshen1987/subnucleondiffraction subnucleondiffraction_code
+git clone --depth=1 https://github.com/hejajama/subnucleondiffraction subnucleondiffraction_code
+#git clone --depth=1 https://github.com/chunshen1987/subnucleondiffraction subnucleondiffraction_code
 (cd subnucleondiffraction_code; git checkout e90c55fac4f51e48b08a29739cc06e97144f0b86)
 rm -fr subnucleondiffraction_code/.git
 
